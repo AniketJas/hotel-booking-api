0bdb63ea225addb29d432c5afda5b67e5a5d8174
Aniket Jas <aniket.jas20@gmail.com>
2026-04-15 21:52:37 +0530

    revert changes for folder name

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,20 +4,9 @@ import cloudinary from "../configs/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: async (req, file) => {
-    const isProd = process.env.NODE_ENV === "production";
-
-    const placeId = req.body.placeId || "general";
-
-    return {
-      folder: `stayzy/${isProd ? "prod" : "dev"}/uploads`,
-      allowed_formats: ["jpg", "png", "jpeg", "webp"],
-
-      // 🔥 KEY PART
-      public_id: `place_${placeId}_${Date.now()}`,
-
-      resource_type: "image",
-    };
+  params: {
+    folder: "stayzy-hotel-booking/uploads",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
   },
 });
 
